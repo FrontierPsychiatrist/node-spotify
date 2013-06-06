682ac713995c17550dd05f99423d86ee8e870ef5
Moritz Schulze <moritz.schulze@gmail.com>
2013-06-06 21:12:00 +0200

    yeoman initial configuration

---
+++ frontend2/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('frontend2App', [])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ frontend2/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('frontend2App')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
