12397240fdbde503485c79aa84976ed3a120f837
Moritz Schulze <moritz.schulze@gmail.com>
2013-06-15 12:52:42 +0200

    Added login window

--- frontend/app/scripts/controllers/Main.js
+++ frontend/app/scripts/controllers/Main.js
@@ -1,15 +1,20 @@
 'use strict';
 /* jshint camelcase: false */
+/* global $ */
 angular.module('node-spotify')
   .controller('MainCtrl', function ($scope, socket, events) {
     $scope.logout = function() {
       socket.emit(events.logout);
     };
+    $scope.login = function(username, password) {
+      socket.emit(events.login, {username: username, password: password});
+    };
     socket.on(events.logged_in, function(data) {
       if(data.loggedIn) {
+        $('#modal').modal('hide');
         socket.emit(events.initial_data);
       } else {
-
+        $('#modal').modal({keyboard: false});
       }
     });
     socket.emit(events.logged_in);
