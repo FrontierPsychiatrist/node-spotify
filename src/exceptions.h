57719af3337cc7e03d628ef1da4f6fbd718f666b
Moritz Schulze <moritz.schulze@gmail.com>
2014-03-09 13:13:25 +0100

    Added exception macro to create exceptions with an error message

--- src/exceptions.h
+++ src/exceptions.h
@@ -25,21 +25,25 @@ THE SOFTWARE.
 #ifndef _NODESPOTIFY_EXCEPTIONS_H
 #define _NODESPOTIFY_EXCEPTIONS_H
 
+#define CREATE_EXCEPTION_WITH_MESSAGE(name) class name : public ExceptionWithMessage {\
+  public:\
+    name(const char* message) : ExceptionWithMessage(message) {};\
+  };
+
 #include <exception>
 
+class ExceptionWithMessage {
+public:
+  ExceptionWithMessage(const char* _message) : message(_message) {};
+  std::string message;
+};
 class FileException : public std::exception {};
 class TrackNotPlayableException : public std::exception {};
 class PlaylistCreationException : public std::exception {};
 class PlaylistNotDeleteableException : public std::exception {};
-class PlaylistNotMoveableException : public std::exception {
-public:
-  PlaylistNotMoveableException(const char* _message) : message(_message) {};
-  std::string message;
-};
-class SessionCreationException : public std::exception {
-public:
-  SessionCreationException(const char* _message) : message(_message) {};
-  std::string message;
-};
+
+CREATE_EXCEPTION_WITH_MESSAGE(PlaylistNotMoveableException)
+CREATE_EXCEPTION_WITH_MESSAGE(TracksNotReorderableException)
+CREATE_EXCEPTION_WITH_MESSAGE(SessionCreationException)
 
 #endif
