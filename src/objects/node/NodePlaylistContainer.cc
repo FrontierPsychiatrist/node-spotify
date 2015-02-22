4ae11b43fb7744297e6d77f3dd9d53fffd17c83c
Moritz Schulze <moritz@frontierpsychiatrist.de>
2015-02-22 15:44:48 +0100

    NodePlaylistContainer compiles

--- src/objects/node/NodePlaylistContainer.cc
+++ src/objects/node/NodePlaylistContainer.cc
@@ -13,28 +13,28 @@ NodePlaylistContainer::NodePlaylistContainer(std::shared_ptr<PlaylistContainer> 
 NodePlaylistContainer::~NodePlaylistContainer() {
 }
 
-Handle<Value> NodePlaylistContainer::getOwner(Local<String> property, const AccessorInfo& info) {
-  HandleScope scope;
-  NodePlaylistContainer* nodePlaylistContainer = node::ObjectWrap::Unwrap<NodePlaylistContainer>(info.Holder());
+NAN_GETTER(NodePlaylistContainer::getOwner) {
+  NanScope();
+  NodePlaylistContainer* nodePlaylistContainer = node::ObjectWrap::Unwrap<NodePlaylistContainer>(args.This());
   NodeUser* nodeUser = new NodeUser(nodePlaylistContainer->playlistContainer->owner());
-  return scope.Close(nodeUser->getV8Object());
+  NanReturnValue(nodeUser->getV8Object());
 }
 
-Handle<Value> NodePlaylistContainer::getNumPlaylists(Local<String> property, const AccessorInfo& info) {
-  HandleScope scope;
-  NodePlaylistContainer* nodePlaylistContainer = node::ObjectWrap::Unwrap<NodePlaylistContainer>(info.Holder());
-  return scope.Close(Integer::New(nodePlaylistContainer->playlistContainer->numPlaylists()));
+NAN_GETTER(NodePlaylistContainer::getNumPlaylists) {
+  NanScope();
+  NodePlaylistContainer* nodePlaylistContainer = node::ObjectWrap::Unwrap<NodePlaylistContainer>(args.This());
+  NanReturnValue(NanNew<Integer>(nodePlaylistContainer->playlistContainer->numPlaylists()));
 }
 
-Handle<Value> NodePlaylistContainer::getPlaylist(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(NodePlaylistContainer::getPlaylist) {
+  NanScope();
   if(args.Length() < 1 || !args[0]->IsNumber()) {
-    return scope.Close(V8_EXCEPTION("getPlaylist needs an interger as its first argument."));
+    NanThrowError("getPlaylist needs an interger as its first argument.");
   }
   int index = args[0]->ToNumber()->IntegerValue();
   NodePlaylistContainer* nodePlaylistContainer = node::ObjectWrap::Unwrap<NodePlaylistContainer>(args.This());
   if(index < 0 || index > nodePlaylistContainer->playlistContainer->numPlaylists()) {
-    return scope.Close(V8_EXCEPTION("Index out of range."));
+    NanThrowError("Index out of range.");
   }
   std::shared_ptr<PlaylistBase> playlist = nodePlaylistContainer->playlistContainer->getPlaylist(index);
 
@@ -47,28 +47,28 @@ Handle<Value> NodePlaylistContainer::getPlaylist(const Arguments& args) {
     outNodePlaylist = nodePlaylistFolder->getV8Object();
   }
 
-  return scope.Close(outNodePlaylist);
+  NanReturnValue(outNodePlaylist);
 }
 
-Handle<Value> NodePlaylistContainer::addPlaylist(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(NodePlaylistContainer::addPlaylist) {
+  NanScope();
   if(args.Length() < 1 || !args[0]->IsString()) {
-    return scope.Close(V8_EXCEPTION("addPlaylist needs a string as its argument"));
+    NanThrowError("addPlaylist needs a string as its argument");
   }
   NodePlaylistContainer* nodePlaylistContainer = node::ObjectWrap::Unwrap<NodePlaylistContainer>(args.This());
   String::Utf8Value playlistName(args[0]->ToString());
   try {
     nodePlaylistContainer->playlistContainer->addPlaylist(std::string(*playlistName));
   } catch(const PlaylistCreationException& e) {
-    return scope.Close(V8_EXCEPTION("Playlist creation failed"));
+    NanThrowError("Playlist creation failed");
   }
-  return scope.Close(Undefined());
+  NanReturnUndefined();
 }
 
-Handle<Value> NodePlaylistContainer::addFolder(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(NodePlaylistContainer::addFolder) {
+  NanScope();
   if(args.Length() < 2 || !args[0]->IsNumber() || !args[1]->IsString()) {
-    return scope.Close(V8_EXCEPTION("addFolder needs a number and a string as arguments."));
+    NanThrowError("addFolder needs a number and a string as arguments.");
   }
   NodePlaylistContainer* nodePlaylistContainer = node::ObjectWrap::Unwrap<NodePlaylistContainer>(args.This());
   int index = args[0]->ToNumber()->IntegerValue();
@@ -76,26 +76,26 @@ Handle<Value> NodePlaylistContainer::addFolder(const Arguments& args) {
   try {
     nodePlaylistContainer->playlistContainer->addFolder(index, std::string(*folderName));
   } catch(const PlaylistCreationException& e) {
-    return scope.Close(V8_EXCEPTION("Folder creation failed"));
+    NanThrowError("Folder creation failed");
   }
-  return scope.Close(Undefined());
+  NanReturnUndefined();
 }
 
-Handle<Value> NodePlaylistContainer::deletePlaylist(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(NodePlaylistContainer::deletePlaylist) {
+  NanScope();
   if(args.Length() < 1 || !args[0]->IsNumber()) {
-    return scope.Close(V8_EXCEPTION("deletePlaylist needs an integer as its first argument."));
+    NanThrowError("deletePlaylist needs an integer as its first argument.");
   }
   NodePlaylistContainer* nodePlaylistContainer = node::ObjectWrap::Unwrap<NodePlaylistContainer>(args.This());
   int position = args[0]->ToNumber()->IntegerValue();
   nodePlaylistContainer->playlistContainer->removePlaylist(position);
-  return scope.Close(Undefined());
+  NanReturnUndefined();
 }
 
-Handle<Value> NodePlaylistContainer::movePlaylist(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(NodePlaylistContainer::movePlaylist) {
+  NanScope();
   if(args.Length() < 2 || !args[0]->IsNumber() || !args[1]->IsNumber()) {
-    return scope.Close(V8_EXCEPTION("Move playlist needs 2 numbers as its first arguments."));
+    NanThrowError("Move playlist needs 2 numbers as its first arguments.");
   }
   int index = args[0]->ToNumber()->IntegerValue();
   int newPosition = args[1]->ToNumber()->IntegerValue();
@@ -103,56 +103,56 @@ Handle<Value> NodePlaylistContainer::movePlaylist(const Arguments& args) {
   try {
     nodePlaylistContainer->playlistContainer->movePlaylist(index, newPosition);
   } catch(const PlaylistNotMoveableException& e) {
-    return scope.Close(V8_EXCEPTION(e.message.c_str()));
+    NanThrowError(e.message.c_str());
   }
-  return scope.Close(Undefined());
+  NanReturnUndefined();
 }
 
-Handle<Value> NodePlaylistContainer::isLoaded(Local<String> property, const AccessorInfo& info) {
-  NodePlaylistContainer* nodePlaylistContainer = node::ObjectWrap::Unwrap<NodePlaylistContainer>(info.Holder());
-  return Boolean::New(nodePlaylistContainer->playlistContainer->isLoaded());
+NAN_GETTER(NodePlaylistContainer::isLoaded) {
+  NanScope();
+  NodePlaylistContainer* nodePlaylistContainer = node::ObjectWrap::Unwrap<NodePlaylistContainer>(args.This());
+  NanReturnValue(NanNew<Boolean>(nodePlaylistContainer->playlistContainer->isLoaded()));
 }
 
-Handle<Value> NodePlaylistContainer::on(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(NodePlaylistContainer::on) {
+  NanScope();
   if(args.Length() < 1 || !args[0]->IsObject()) {
-    return scope.Close(V8_EXCEPTION("on needs an object as its first argument."));
+    NanThrowError("on needs an object as its first argument.");
   }
   NodePlaylistContainer* nodePlaylistContainer = node::ObjectWrap::Unwrap<NodePlaylistContainer>(args.This());
   Handle<Object> callbacks = args[0]->ToObject();
-  Handle<String> playlistAddedKey = String::New("playlistAdded");
-  Handle<String> playlistMovedKey = String::New("playlistMoved");
-  Handle<String> playlistRemovedKey = String::New("playlistRemoved");
+  Handle<String> playlistAddedKey = NanNew<String>("playlistAdded");
+  Handle<String> playlistMovedKey = NanNew<String>("playlistMoved");
+  Handle<String> playlistRemovedKey = NanNew<String>("playlistRemoved");
   nodePlaylistContainer->playlistContainerCallbacksHolder.playlistAddedCallback = V8Utils::getFunctionFromObject(callbacks, playlistAddedKey);
   nodePlaylistContainer->playlistContainerCallbacksHolder.playlistMovedCallback = V8Utils::getFunctionFromObject(callbacks, playlistMovedKey);
   nodePlaylistContainer->playlistContainerCallbacksHolder.playlistRemovedCallback = V8Utils::getFunctionFromObject(callbacks, playlistRemovedKey);
   nodePlaylistContainer->playlistContainerCallbacksHolder.setCallbacks();
-  return scope.Close(Undefined());
+  NanReturnUndefined();
 }
 
-Handle<Value> NodePlaylistContainer::off(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(NodePlaylistContainer::off) {
+  NanScope();
   NodePlaylistContainer* nodePlaylistContainer = node::ObjectWrap::Unwrap<NodePlaylistContainer>(args.This());
   nodePlaylistContainer->playlistContainerCallbacksHolder.unsetCallbacks();
-  return scope.Close(Undefined());
+  NanReturnUndefined();
 }
 
 void NodePlaylistContainer::init() {
-  HandleScope scope;
-  Local<FunctionTemplate> constructorTemplate = FunctionTemplate::New();
-  constructorTemplate->SetClassName(String::NewSymbol("PlaylistContainer"));
+  NanScope();
+  Local<FunctionTemplate> constructorTemplate = NanNew<FunctionTemplate>();
+  constructorTemplate->SetClassName(NanNew<String>("PlaylistContainer"));
   constructorTemplate->InstanceTemplate()->SetInternalFieldCount(1);
   NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "on", on);
   NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "off", off);
-  constructorTemplate->InstanceTemplate()->SetAccessor(String::NewSymbol("owner"), getOwner);
-  constructorTemplate->InstanceTemplate()->SetAccessor(String::NewSymbol("numPlaylists"), getNumPlaylists);
-  constructorTemplate->InstanceTemplate()->SetAccessor(String::NewSymbol("isLoaded"), isLoaded);
+  constructorTemplate->InstanceTemplate()->SetAccessor(NanNew<String>("owner"), getOwner);
+  constructorTemplate->InstanceTemplate()->SetAccessor(NanNew<String>("numPlaylists"), getNumPlaylists);
+  constructorTemplate->InstanceTemplate()->SetAccessor(NanNew<String>("isLoaded"), isLoaded);
   NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "getPlaylist", getPlaylist);
   NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "addPlaylist", addPlaylist);
   NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "addFolder", addFolder);
   NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "deletePlaylist", deletePlaylist);
   NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "movePlaylist", movePlaylist);
 
-  constructor = Persistent<Function>::New(constructorTemplate->GetFunction());
-  scope.Close(Undefined());
+  constructor = constructorTemplate->GetFunction();
 }
