0516021822e0caefe45f7e054d7256c0548eabfa
Moritz Schulze <moritz@frontierpsychiatrist.de>
2015-02-22 14:44:52 +0100

    NodePlayer compiles

--- src/objects/node/NodePlayer.cc
+++ src/objects/node/NodePlayer.cc
@@ -15,85 +15,86 @@ NodePlayer::NodePlayer(const NodePlayer& other) {
 
 }
 
-Handle<Value> NodePlayer::pause(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(NodePlayer::pause) {
+  NanScope();
   NodePlayer* nodePlayer = node::ObjectWrap::Unwrap<NodePlayer>(args.This());
   nodePlayer->player->pause();
-  return scope.Close(Undefined());
+  NanReturnUndefined();
 }
 
-Handle<Value> NodePlayer::stop(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(NodePlayer::stop) {
+  NanScope();
   NodePlayer* nodePlayer = node::ObjectWrap::Unwrap<NodePlayer>(args.This());
   nodePlayer->player->stop();
-  return scope.Close(Undefined());
+  NanReturnUndefined();
 }
 
-Handle<Value> NodePlayer::resume(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(NodePlayer::resume) {
+  NanScope();
   NodePlayer* nodePlayer = node::ObjectWrap::Unwrap<NodePlayer>(args.This());
   nodePlayer->player->resume();
-  return scope.Close(Undefined());
+  NanReturnUndefined();
 }
 
-Handle<Value> NodePlayer::play(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(NodePlayer::play) {
+  NanScope();
   if(args.Length() < 1) {
-    return scope.Close(V8_EXCEPTION("play needs a track as its first argument."));
+    NanThrowError("play needs a track as its first argument.");
   }
   NodePlayer* nodePlayer = node::ObjectWrap::Unwrap<NodePlayer>(args.This());
   NodeTrack* nodeTrack = node::ObjectWrap::Unwrap<NodeTrack>(args[0]->ToObject());
   try {
     nodePlayer->player->play(nodeTrack->track);
   } catch (const TrackNotPlayableException& e) {
-    return scope.Close(V8_EXCEPTION("Track not playable"));
+    NanThrowError("Track not playable");
   }
 #ifndef NODE_SPOTIFY_NATIVE_SOUND
   catch (const NoAudioHandlerException& e) {
-    return scope.Close(V8_EXCEPTION("No audio handler registered. Use spotify.useNodejsAudio()."));
+    NanThrowError("No audio handler registered. Use spotify.useNodejsAudio().");
   }
 #endif
 
-  return scope.Close(Undefined());
+  NanReturnUndefined();
 }
 
-Handle<Value> NodePlayer::seek(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(NodePlayer::seek) {
+  NanScope();
   if(args.Length() < 1 || !args[0]->IsNumber()) {
-    return scope.Close(V8_EXCEPTION("seek needs an integer as its first argument."));
+    NanThrowError("seek needs an integer as its first argument.");
   }
   NodePlayer* nodePlayer = node::ObjectWrap::Unwrap<NodePlayer>(args.This());
   int second = args[0]->ToInteger()->Value();
   nodePlayer->player->seek(second);
-  return scope.Close(Undefined());
+  NanReturnUndefined();
 }
 
-Handle<Value> NodePlayer::getCurrentSecond(Local<String> property, const AccessorInfo& info) {
-  NodePlayer* nodePlayer = node::ObjectWrap::Unwrap<NodePlayer>(info.Holder());
-  return Integer::New(nodePlayer->player->currentSecond);
+NAN_GETTER(NodePlayer::getCurrentSecond) {
+  NanScope();
+  NodePlayer* nodePlayer = node::ObjectWrap::Unwrap<NodePlayer>(args.This());
+  NanReturnValue(NanNew<Integer>(nodePlayer->player->currentSecond));
 }
 
-Handle<Value> NodePlayer::on(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(NodePlayer::on) {
+  NanScope();
   if(args.Length() < 1 || !args[0]->IsObject()) {
-    return scope.Close(V8_EXCEPTION("on needs an object as its first argument."));
+    NanThrowError("on needs an object as its first argument.");
   }
   Handle<Object> callbacks = args[0]->ToObject();
-  Handle<String> endOfTrackKey = String::New("endOfTrack");
+  Handle<String> endOfTrackKey = NanNew<String>("endOfTrack");
   SessionCallbacks::endOfTrackCallback = V8Utils::getFunctionFromObject(callbacks, endOfTrackKey);
-  return scope.Close(Undefined());
+  NanReturnUndefined();
 }
 
-Handle<Value> NodePlayer::off(const Arguments& args) {
-  HandleScope scope;
-  SessionCallbacks::endOfTrackCallback = Handle<Function>();
-  return scope.Close(Undefined());
+NAN_METHOD(NodePlayer::off) {
+  NanScope();
+  SessionCallbacks::endOfTrackCallback = std::unique_ptr<NanCallback>(new NanCallback());
+  NanReturnUndefined();
 }
 
 void NodePlayer::init() {
-  HandleScope scope;
-  Local<FunctionTemplate> constructorTemplate = FunctionTemplate::New();
-  constructorTemplate->SetClassName(String::NewSymbol("Player"));
+  NanScope();
+  Local<FunctionTemplate> constructorTemplate = NanNew<FunctionTemplate>();
+  constructorTemplate->SetClassName(NanNew<String>("Player"));
   constructorTemplate->InstanceTemplate()->SetInternalFieldCount(1);
   NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "on", on);
   NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "off", off);
@@ -103,7 +104,6 @@ void NodePlayer::init() {
   NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "resume", resume);
   NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "stop", stop);
   NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "seek", seek);
-  constructorTemplate->InstanceTemplate()->SetAccessor(String::NewSymbol("currentSecond"), &getCurrentSecond);
-  constructor = Persistent<Function>::New(constructorTemplate->GetFunction());
-  scope.Close(Undefined());
+  constructorTemplate->InstanceTemplate()->SetAccessor(NanNew<String>("currentSecond"), &getCurrentSecond);
+  constructor = constructorTemplate->GetFunction();
 }
