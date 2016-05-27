a8976885e05ec8404a0447e2e2ba5c373aa15801
Moritz Schulze <moritz@frontierpsychiatrist.de>
2016-05-28 00:27:19 +0200

    Fix NodePlayer return values and init method.
    TODO: The off method does not do anything anymore! How to reset a Nan::Callback to empty?

--- src/objects/node/NodePlayer.cc
+++ src/objects/node/NodePlayer.cc
@@ -17,84 +17,88 @@ NodePlayer::NodePlayer(const NodePlayer& other) {
 NAN_METHOD(NodePlayer::pause) {
   NodePlayer* nodePlayer = node::ObjectWrap::Unwrap<NodePlayer>(info.This());
   nodePlayer->player->pause();
-  NanReturnUndefined();
+  info.GetReturnValue().SetUndefined();
 }
 
 NAN_METHOD(NodePlayer::stop) {
   NodePlayer* nodePlayer = node::ObjectWrap::Unwrap<NodePlayer>(info.This());
   nodePlayer->player->stop();
-  NanReturnUndefined();
+  info.GetReturnValue().SetUndefined();
 }
 
 NAN_METHOD(NodePlayer::resume) {
   NodePlayer* nodePlayer = node::ObjectWrap::Unwrap<NodePlayer>(info.This());
   nodePlayer->player->resume();
-  NanReturnUndefined();
+  info.GetReturnValue().SetUndefined();
 }
 
 NAN_METHOD(NodePlayer::play) {
-  if(args.Length() < 1) {
-    return NanThrowError("play needs a track as its first argument.");
+  if(info.Length() < 1) {
+    Nan::ThrowError("play needs a track as its first argument.");
+    return;
   }
   NodePlayer* nodePlayer = node::ObjectWrap::Unwrap<NodePlayer>(info.This());
-  NodeTrack* nodeTrack = node::ObjectWrap::Unwrap<NodeTrack>(args[0]->ToObject());
+  NodeTrack* nodeTrack = node::ObjectWrap::Unwrap<NodeTrack>(info[0]->ToObject());
   try {
     nodePlayer->player->play(nodeTrack->track);
   } catch (const TrackNotPlayableException& e) {
-    return NanThrowError("Track not playable");
+    Nan::ThrowError("Track not playable");
+    return;
   }
 #ifndef NODE_SPOTIFY_NATIVE_SOUND
   catch (const NoAudioHandlerException& e) {
-    return NanThrowError("No audio handler registered. Use spotify.useNodejsAudio().");
+    Nan::ThrowError("No audio handler registered. Use spotify.useNodejsAudio().");
+    return;
   }
 #endif
 
-  NanReturnUndefined();
+  info.GetReturnValue().SetUndefined();
 }
 
 NAN_METHOD(NodePlayer::seek) {
-  if(args.Length() < 1 || !args[0]->IsNumber()) {
-    return NanThrowError("seek needs an integer as its first argument.");
+  if(info.Length() < 1 || !info[0]->IsNumber()) {
+    Nan::ThrowError("seek needs an integer as its first argument.");
+    return;
   }
   NodePlayer* nodePlayer = node::ObjectWrap::Unwrap<NodePlayer>(info.This());
-  int second = args[0]->ToInteger()->Value();
+  int second = info[0]->ToInteger()->Value();
   nodePlayer->player->seek(second);
-  NanReturnUndefined();
+  info.GetReturnValue().SetUndefined();
 }
 
 NAN_GETTER(NodePlayer::getCurrentSecond) {
   NodePlayer* nodePlayer = node::ObjectWrap::Unwrap<NodePlayer>(info.This());
-  NanReturnValue(NanNew<Integer>(nodePlayer->player->currentSecond));
+  info.GetReturnValue().Set(Nan::New<Integer>(nodePlayer->player->currentSecond));
 }
 
 NAN_METHOD(NodePlayer::on) {
-  if(args.Length() < 1 || !args[0]->IsObject()) {
-    return NanThrowError("on needs an object as its first argument.");
+  if(info.Length() < 1 || !info[0]->IsObject()) {
+    Nan::ThrowError("on needs an object as its first argument.");
+    return;
   }
-  Handle<Object> callbacks = args[0]->ToObject();
+  Handle<Object> callbacks = info[0]->ToObject();
   Handle<String> endOfTrackKey = Nan::New<String>("endOfTrack").ToLocalChecked();
-  SessionCallbacks::endOfTrackCallback = V8Utils::getFunctionFromObject(callbacks, endOfTrackKey);
-  NanReturnUndefined();
+  SessionCallbacks::endOfTrackCallback.SetFunction(V8Utils::getFunctionFromObject(callbacks, endOfTrackKey));
+  info.GetReturnValue().SetUndefined();
 }
 
 NAN_METHOD(NodePlayer::off) {
-  SessionCallbacks::endOfTrackCallback = std::unique_ptr<NanCallback>(new NanCallback());
-  NanReturnUndefined();
+  //SessionCallbacks::endOfTrackCallback = Nan::Callback();
+  info.GetReturnValue().SetUndefined();
 }
 
 void NodePlayer::init() {
-  NanScope();
-  Local<FunctionTemplate> constructorTemplate = NanNew<FunctionTemplate>();
+  Local<FunctionTemplate> constructorTemplate = Nan::New<FunctionTemplate>();
   constructorTemplate->SetClassName(Nan::New<String>("Player").ToLocalChecked());
   constructorTemplate->InstanceTemplate()->SetInternalFieldCount(1);
-  NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "on", on);
-  NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "off", off);
+  Nan::SetMethod(constructorTemplate, "on", on);
+  Nan::SetMethod(constructorTemplate, "off", off);
 
-  NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "play", play);
-  NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "pause", pause);
-  NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "resume", resume);
-  NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "stop", stop);
-  NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "seek", seek);
-  constructorTemplate->InstanceTemplate()->SetAccessor(Nan::New<String>("currentSecond").ToLocalChecked(), &getCurrentSecond);
-  NanAssignPersistent(NodePlayer::constructorTemplate, constructorTemplate);
+  Nan::SetMethod(constructorTemplate, "play", play);
+  Nan::SetMethod(constructorTemplate, "pause", pause);
+  Nan::SetMethod(constructorTemplate, "resume", resume);
+  Nan::SetMethod(constructorTemplate, "stop", stop);
+  Nan::SetMethod(constructorTemplate, "seek", seek);
+  Nan::SetAccessor(constructorTemplate->InstanceTemplate(), Nan::New<String>("currentSecond").ToLocalChecked(), &getCurrentSecond);
+  NodePlayer::constructorTemplate.Reset(constructorTemplate);
 }
